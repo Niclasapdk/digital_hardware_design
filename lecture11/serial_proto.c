615dff3963b80e311ddbda5af2abdfc295702666
Markus Heinrich Toribio <23717111+mhtoribio@users.noreply.github.com>
2024-01-13 16:38:53 +0100

    finish lecture 11 exercise

--- lecture11/serial_proto.c
+++ lecture11/serial_proto.c
@@ -2,12 +2,29 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdbool.h>
+#include <errno.h>
 
+/* **** User Interface **** */
 #define PROMPT "> "
+/* **** User Interface **** */
 
+/* **** PROTOCOL **** */
 #define FRAME_DELIM 'S'
-#define ACK 0x06
+#define DATA_DELIM ','
+#define ACK 'Y'
 
+// Command mnemonics
+#define CMD_LEN 3
+#define CMD_READ  "RDA"
+#define CMD_WRITE "WRA"
+
+// Number of digits
+#define PORT_NUM_DIGITS 2
+#define VAL_NUM_DIGITS 4
+/* **** PROTOCOL **** */
+
+/* **** Serial Emulation **** */
 #define BUFSIZE 64 // max 256
 typedef struct msgBuf {
     char name[8]; // name for debugging. Max length 7 bytes
@@ -16,34 +33,67 @@ typedef struct msgBuf {
     int16_t dataWaiting;
     char data[BUFSIZE];
 } msgBuf_t;
+/* **** Serial Emulation **** */
+
+/* **** Slave Port Emulation **** */
+#define NUM_PORTS 100
+typedef struct portArray {
+    uint16_t ports[NUM_PORTS];
+} portArray_t;
+/* **** Slave Port Emulation **** */
+
+void portsDbgShow(portArray_t *portArr);
+void bufDbgShow(msgBuf_t *buf);
 
 int bufSendByte(msgBuf_t *buf, uint8_t data); // returns negative number if ERR
 uint8_t bufRecvByte(msgBuf_t *buf);
-
-void dbgShowBuf(msgBuf_t *buf);
 
 size_t getCommand(char *inpBuf, int maxSize); // returns number of bytes read
 
 void bufSendFrame(msgBuf_t *buf, char *data, int n);
 size_t bufRecvFrame(msgBuf_t *buf, char *data, int maxLen);
 
+int slaveParseCommand(portArray_t *portArr, msgBuf_t *buf, char *cmdBuf);
+
 int main() {
     msgBuf_t *txBuf = calloc(1, sizeof(msgBuf_t));
     strcpy(txBuf->name, "txBuf");
     msgBuf_t *rxBuf = calloc(1, sizeof(msgBuf_t));
     strcpy(rxBuf->name, "rxBuf");
+    portArray_t *portArr = calloc(1, sizeof(portArray_t));
 
     char inpBuf[BUFSIZE];
     char recvBuf[BUFSIZE];
-    size_t cmdLen;
     while (1) {
-        cmdLen = getCommand(inpBuf, BUFSIZE); // master
-        //printf("cmdLen: %llu, inpBuf: %s\n", cmdLen, inpBuf);
-        bufSendFrame(txBuf, inpBuf, cmdLen); // master
-        dbgShowBuf(txBuf); // master
-        size_t count = bufRecvFrame(txBuf, recvBuf, BUFSIZE); // slave
-        printf("Slave receive buf: count: %llu, data: %s\n", count, recvBuf); // slave
+        portsDbgShow(portArr);
+        bufDbgShow(txBuf);
+        bufDbgShow(rxBuf);
+
+        // Master
+        // Receive stuff
+        size_t parrotLen = bufRecvFrame(rxBuf, recvBuf, BUFSIZE);
+        printf("Master receive buf: count: %llu, data: %s\n\n", parrotLen, recvBuf);
+        // Get command from prompt and send to slave
+        size_t cmdLen = getCommand(inpBuf, BUFSIZE);
+        bufSendFrame(txBuf, inpBuf, cmdLen);
+
+        // Slave
+        size_t recvCmdLen = bufRecvFrame(txBuf, recvBuf, BUFSIZE);
+        printf("\nSlave receive buf: count: %llu, data: %s\n\n", recvCmdLen, recvBuf);
+        slaveParseCommand(portArr, rxBuf, recvBuf);
     }
+}
+
+void portsDbgShow(portArray_t *portArr) {
+    printf("Analog ports debug\n");
+    for (int i = 0; i < NUM_PORTS; i++) {
+        printf("%04u;", portArr->ports[i]);
+        if (i % 10 == 9)
+            putc('\n', stdout);
+        else
+            putc(' ', stdout);
+    }
+    putc('\n', stdout);
 }
 
 int bufSendByte(msgBuf_t *buf, uint8_t data) {
@@ -51,21 +101,25 @@ int bufSendByte(msgBuf_t *buf, uint8_t data) {
         buf->txIdx = 1;
     else
         buf->txIdx++;
+    buf->dataWaiting++;
     buf->data[buf->txIdx-1] = data;
 }
 
 uint8_t bufRecvByte(msgBuf_t *buf) {
+    if (!buf->dataWaiting)
+        return 0xff; // Error
     if (buf->rxIdx >= BUFSIZE)
         buf->rxIdx = 1;
     else
         buf->rxIdx++;
+    buf->dataWaiting--;
     return buf->data[buf->rxIdx-1];
 }
 
-void dbgShowBuf(msgBuf_t *buf) {
-    printf("\n%s debug\ntxIdx: %hu, rxIdx: %hu, data: %s\n", buf->name, buf->txIdx, buf->rxIdx, buf->data);
+void bufDbgShow(msgBuf_t *buf) {
+    printf("%s debug\ntxIdx: %hu, rxIdx: %hu, data: %s\n", buf->name, buf->txIdx, buf->rxIdx, buf->data);
     for (int i = 0; i < BUFSIZE; i++) {
-        printf("%p (%1$c);", buf->data[i]);
+        printf("%1$p (%1$c);", buf->data[i]);
         if (i % 8 == 7)
             putc('\n', stdout);
         else
@@ -95,6 +149,7 @@ void bufSendFrame(msgBuf_t *buf, char *data, int n) {
 }
 
 size_t bufRecvFrame(msgBuf_t *buf, char *data, int maxLen) {
+    memset(data, 0, maxLen);
     size_t count;
     if (bufRecvByte(buf) != FRAME_DELIM)
         return 0;
@@ -108,4 +163,53 @@ size_t bufRecvFrame(msgBuf_t *buf, char *data, int maxLen) {
         }
     }
     return count;
+}
+
+int slaveParseCommand(portArray_t *portArr, msgBuf_t *buf, char *cmdBuf) {
+    int ret = 0;
+    char *endptr;
+
+    // check command type
+    bool isRead = strncmp(cmdBuf, CMD_READ, CMD_LEN) == 0;
+    bool isWrite = strncmp(cmdBuf, CMD_WRITE, CMD_LEN) == 0;
+    cmdBuf += CMD_LEN;
+    // check for data delimiter
+    if (*cmdBuf++ != DATA_DELIM) goto EXIT_ERR;
+
+    // parse port number
+    uint8_t port = strtoul(cmdBuf, &endptr, 10);
+    if (endptr - cmdBuf != PORT_NUM_DIGITS) goto EXIT_ERR;
+    cmdBuf += PORT_NUM_DIGITS;
+    if (port >= NUM_PORTS) goto EXIT_ERR;
+
+    if (isRead) {
+        uint16_t value = portArr->ports[port];
+        // do your best parrot impression
+        char frameBuf[CMD_LEN + PORT_NUM_DIGITS + VAL_NUM_DIGITS + 8] = {0};
+        int n = sprintf(frameBuf, "%c"CMD_READ",%02u,%04u", ACK, port, value);
+        bufSendFrame(buf, frameBuf, n);
+        goto EXIT;
+    } else if (isWrite) {
+        // check for data delimiter
+        if (*cmdBuf++ != DATA_DELIM) goto EXIT_ERR;
+
+        // parse value number
+        uint16_t value = strtoul(cmdBuf, &endptr, 10);
+        if (endptr - cmdBuf != VAL_NUM_DIGITS) goto EXIT_ERR;
+        cmdBuf += VAL_NUM_DIGITS;
+
+        // write to port
+        portArr->ports[port] = value;
+
+        // do your best parrot impression
+        char frameBuf[CMD_LEN + PORT_NUM_DIGITS + 8] = {0};
+        int n = sprintf(frameBuf, "%c"CMD_WRITE",%d", ACK, port);
+        bufSendFrame(buf, frameBuf, n);
+        goto EXIT;
+    }
+EXIT_ERR:
+    // Malformed packet (we could use this for proper error handling but why bother)
+    ret = -EINVAL;
+EXIT:
+    return ret;
 }
